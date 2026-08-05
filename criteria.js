89591780b04c5a2d83e88056457f799e1a255085
haruyoty <yoshiharu.noyama@gmail.com>
2026-08-05 17:52:23 +0800

    Add files via upload

--- criteria.js
+++ criteria.js
@@ -822,7 +822,7 @@ const CONTACT_LINKS = [
        （デコードすると https://noyamagolf.com/オンラインレッスン/ ） */
     url: 'https://noyamagolf.com/%e3%82%aa%e3%83%b3%e3%83%a9%e3%82%a4%e3%83%b3%e3%83%ac%e3%83%83%e3%82%b9%e3%83%b3/',
     title: 'オンラインスイング診断',
-    lead: 'LINE にスイング動画をお送りいただければ、私が直接見て、もっと詳しく診断いたします。'
+    lead: 'LINE にスイング動画をお送りいただければ、直接見て、もっと詳しく診断いたします。'
   },
   {
     url: 'https://noyamagolf.com/?p=47',
